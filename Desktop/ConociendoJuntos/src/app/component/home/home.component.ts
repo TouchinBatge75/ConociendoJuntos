f5dd59867b1b741beaee10a7492eb05261aeb8a9
TouchinBatge75 <elmemes512@hotmail.com>
2025-06-07 23:37:05 -0600

    Se puso el logo en el NavBar

--- Desktop/ConociendoJuntos/src/app/component/home/home.component.ts
+++ Desktop/ConociendoJuntos/src/app/component/home/home.component.ts
@@ -8,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+imagenes = [
+    'assets/img1.jpg',
+    'assets/img2.jpg',
+    'assets/img3.jpg',
+    // Pon aquí las rutas de tus imágenes
+  ];
+  imagenActual = 0;
 
+  anterior() {
+    this.imagenActual = (this.imagenActual === 0) ? this.imagenes.length - 1 : this.imagenActual - 1;
+  }
+
+  siguiente() {
+    this.imagenActual = (this.imagenActual === this.imagenes.length - 1) ? 0 : this.imagenActual + 1;
+  }
 }
