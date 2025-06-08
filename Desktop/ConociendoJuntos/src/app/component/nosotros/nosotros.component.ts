15fdf76df6b8f43f2fbc3867abfb7136b946352f
TouchinBatge75 <elmemes512@hotmail.com>
2025-06-07 19:25:07 -0600

    Se trabajo en el HTML del componente Home

---
+++ Desktop/ConociendoJuntos/src/app/component/nosotros/nosotros.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nosotros',
+  standalone: true,
+  imports: [],
+  templateUrl: './nosotros.component.html',
+  styleUrls: ['./nosotros.component.css']
+})
+export class NosotrosComponent {
+
+}
